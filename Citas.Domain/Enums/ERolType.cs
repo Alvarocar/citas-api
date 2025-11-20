9e8a63816cd772bc94811cdbc8186c06c5107147
Alvaro Cardozo <alvaro_car6@hotmail.com>
2025-11-20 18:41:38 -0500

    feat: clean rol enum

--- Citas.Domain/Enums/ERolType.cs
+++ Citas.Domain/Enums/ERolType.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Citas.Domain.Enums;
 
-namespace Citas.Domain.Enums
+public enum ERolType
 {
-    public enum ERolType
-    {
-        EMPLOYEE,
-        ADMINISTRATOR,
-    }
+  EMPLOYEE,
+  ADMINISTRATOR,
 }
