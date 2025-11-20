b7fd7b85c4477931d656571cd3a61704a9f14e89
Alvaro Cardozo <alvaro_car6@hotmail.com>
2025-11-20 18:37:47 -0500

    feat: add cors layer

---
+++ Citas.Infrastructure/DependencyInjection/CorsSetup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Citas.Infrastructure.DependencyInjection;
+
+public static class CorsSetup
+{
+  public static IServiceCollection AddCitasCors(this IServiceCollection services, IConfiguration configuration)
+  {
+    var origins = configuration.GetSection("Cors:Origins").Get<string[]>();
+
+    if (origins == null) return services;
+
+    services.AddCors(options =>
+    {
+      options.AddPolicy("AllowClient", policy =>
+      {
+        policy.WithOrigins(origins)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+      });
+    });
+    return services;
+  }
+}
