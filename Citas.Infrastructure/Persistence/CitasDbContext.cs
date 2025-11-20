3bd8afc2166836b4c884bbe5ab868542c8ee33b1
Alvaro Cardozo <alvaro_car6@hotmail.com>
2025-11-20 18:38:51 -0500

    refactor: remove enums from dbcontext and add dependencies

--- Citas.Infrastructure/Persistence/CitasDbContext.cs
+++ Citas.Infrastructure/Persistence/CitasDbContext.cs
@@ -1,33 +1,28 @@
-using Microsoft.EntityFrameworkCore;
 using Citas.Domain.Entities;
-using Citas.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace Citas.Infrastructure.Persistence;
 
 public class CitasDbContext : DbContext
 {
-    public CitasDbContext(DbContextOptions<CitasDbContext> options) : base(options) { }
+  public CitasDbContext(DbContextOptions<CitasDbContext> options) : base(options) { }
 
-    public DbSet<Company> Companies { get; set; } = null!;
-    public DbSet<Employee> Employees { get; set; } = null!;
-    public DbSet<EmployeeSchedule> EmployeeSchedules { get; set; } = null!;
-    public DbSet<EmployeeScheduleException> EmployeeScheduleExceptions { get; set; } = null!;
-    public DbSet<EmployeeService> EmployeeServices { get; set; } = null!;
-    public DbSet<EmployeeScheduleRange> EmployeeScheduleRanges { get; set; } = null!;
-    public DbSet<Position> Positions { get; set; } = null!;
-    public DbSet<Rol> Rols { get; set; } = null!;
-    public DbSet<Service> Services { get; set; } = null!;
-    public DbSet<Reservation> Reservations { get; set; } = null!;
-    public DbSet<Client> Clients { get; set; } = null!;
+  public DbSet<Company> Companies { get; set; } = null!;
+  public DbSet<Employee> Employees { get; set; } = null!;
+  public DbSet<EmployeeSchedule> EmployeeSchedules { get; set; } = null!;
+  public DbSet<EmployeeScheduleException> EmployeeScheduleExceptions { get; set; } = null!;
+  public DbSet<EmployeeService> EmployeeServices { get; set; } = null!;
+  public DbSet<EmployeeScheduleRange> EmployeeScheduleRanges { get; set; } = null!;
+  public DbSet<Position> Positions { get; set; } = null!;
+  public DbSet<Rol> Rols { get; set; } = null!;
+  public DbSet<Service> Services { get; set; } = null!;
+  public DbSet<Reservation> Reservations { get; set; } = null!;
+  public DbSet<Client> Clients { get; set; } = null!;
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.HasPostgresEnum<EDay>(name: "enum__day");
-        modelBuilder.HasPostgresEnum<ERolType>(name: "enum__rol");
-        modelBuilder.HasPostgresEnum<EReservationState>(name: "enum__reservation_state");
-
-        // Apply all configurations from the current project (infrastructure).
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CitasDbContext).Assembly);
-        base.OnModelCreating(modelBuilder);
-    }
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    // Apply all configurations from the current project (infrastructure).
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(CitasDbContext).Assembly);
+    base.OnModelCreating(modelBuilder);
+  }
 }
