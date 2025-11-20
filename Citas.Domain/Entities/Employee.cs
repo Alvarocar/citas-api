224215582030954d7e1bcbbe5dc871dc359ed75a
Alvaro Cardozo <alvaro_car6@hotmail.com>
2025-11-20 18:41:13 -0500

    refactor: fix attributes nulleable behavior

--- Citas.Domain/Entities/Employee.cs
+++ Citas.Domain/Entities/Employee.cs
@@ -1,29 +1,29 @@
 ﻿namespace Citas.Domain.Entities
 {
-    public class Employee
-    {
-        public int Id { get; set; }
+  public class Employee
+  {
+    public int Id { get; set; }
 
-        public required string FirstName { get; set; }
+    public required string FirstName { get; set; }
 
-        public required string LastName { get; set; }
+    public required string LastName { get; set; }
 
-        public required string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 
-        public required bool IsActive { get; set; }
+    public bool IsActive { get; set; }
 
-        public required bool HasAccount { get; set; }
+    public bool HasAccount { get; set; }
 
-        public string? Email { get; set; }
+    public string? Email { get; set; }
 
-        public string? Password { get; set; }
+    public string? Password { get; set; }
 
-        public required Rol Rol { get; set; }
-    
-        public required Company Company { get; set; }
+    public required Rol Rol { get; set; }
 
-        public required EmployeeSchedule Schedule { get; set; }
+    public Company? Company { get; set; }
 
-        public Position? Position { get; set; }
-    }
+    public EmployeeSchedule? Schedule { get; set; }
+
+    public Position? Position { get; set; }
+  }
 }
