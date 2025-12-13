e4b7e8df6e5d5f8a30b0d95d763d5a180f09b75b
devdiegomt <devdiegomt@gmail.com>
2025-12-13 13:47:32 -0500

    fix: cleaning comments.

--- Citas.Application/Dto/ServiceCreateDto.cs
+++ Citas.Application/Dto/ServiceCreateDto.cs
@@ -5,5 +5,5 @@ public class ServiceCreateDto
     public required string Name { get; set; }
     public required string Description { get; set; }
     public float SuggestedPrice { get; set; }
-    public bool IsUnavailable { get; set; } // ¿Es importante acá?
+    public bool IsUnavailable { get; set; }
 }
